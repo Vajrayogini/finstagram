3700581b9c8d8e6974f9df281078043c25f9b893
Vajrayogini <fereig@ualberta.ca>
2022-02-24 00:06:28 +0000

    phew no idea if she'll work

--- app/actions.rb
+++ app/actions.rb
@@ -21,6 +21,7 @@ post '/signup' do
   else
     erb(:signup)
   end
+end
 
   get '/login' do    # when a GET request comes into /login
     erb(:login)      # render app/views/login.erb
@@ -39,6 +40,7 @@ post '/signup' do
       @error_message = "Login failed."
       erb(:login)
     end
+  end
 
     get '/logout' do
       session[:user_id] = nil
@@ -49,4 +51,57 @@ post '/signup' do
     def current_user
       User.find_by(id: session[:user_id])
     end
+
+    get '/finstagram_posts/new' do
+      @finstagram_post = FinstagramPost.new
+      erb(:"finstagram_posts/new")
+    end
+
+    post '/finstagram_posts' do
+      photo_url = params[:photo_url]
+    
+      @finstagram_post = FinstagramPost.new({ photo_url: photo_url, user_id: current_user.id })
+    
+      if @finstagram_post.save
+        redirect(to('/'))
+      else
+        erb(:"finstagram_posts/new")
+      end
+    end
+
+    get '/finstagram_posts/:id' do
+      @finstagram_post = FinstagramPost.find(params[:id])   # find the finstagram post with the ID from the URL
+      erb(:"finstagram_posts/show")               # render app/views/finstagram_posts/show.erb
+    end
+
+    post '/comments' do
+      # point values from params to variables
+      text = params[:text]
+      finstagram_post_id = params[:finstagram_post_id]
+    
+      # instantiate a comment with those values & assign the comment to the `current_user`
+      comment = Comment.new({ text: text, finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+    
+      # save the comment
+      comment.save
+    
+      # `redirect` back to wherever we came from
+      redirect(back)
+    end
+  end
+
+  post '/likes' do
+    finstagram_post_id = params[:finstagram_post_id]
+  
+    like = Like.new({ finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+    like.save
+  
+    redirect(back)
+  end
+
+  delete '/likes/:id' do
+    like = Like.find(params[:id])
+    like.destroy
+    redirect(back)
+  end
 end
